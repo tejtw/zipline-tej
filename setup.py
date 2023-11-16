e608737f1d1aaa1b69572d00c8c6ad5a0a8d254e
TEJ <43775114+tej87681088@users.noreply.github.com>
2023-11-16 17:53:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -102,9 +102,22 @@ ext_modules = [
 ]
 # for ext_module in ext_modules:
 #     ext_module.cython_directives = dict(language_level="3")
+def myversion():
+    from setuptools_scm.version import SEMVER_MINOR, guess_next_simple_semver, release_branch_semver_version
+
+    def my_release_branch_semver_version(version):
+        v = release_branch_semver_version(version)
+        if v == version.format_next_version(guess_next_simple_semver, retain=SEMVER_MINOR):
+            return version.format_next_version(guess_next_simple_semver, fmt="{guessed}", retain=SEMVER_MINOR)
+        return v
+
+    return {
+        'version_scheme': my_release_branch_semver_version,
+        'local_scheme': 'no-local-version',
+    }
 
 setup(
-    use_scm_version=True,
+    use_scm_version=myversion,
     ext_modules=cythonize(ext_modules, **ext_options),
     include_dirs=[numpy.get_include()],
 )
